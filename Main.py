c92580b11eb9f68586f82dc2ba429efa54f6d716
Nikhilkowluru7 <nikhilkowluru@gmail.com>
2026-02-21 12:33:05 +0000

    Space invader

--- Main.py
+++ Main.py
@@ -50,6 +50,29 @@ font = pygame.font.Font("freesansbold.ttf",32)
 textX = 10
 textY = 10
 over_font = pygame.font.Font("freesansbold.ttf",62)
+def show_score(x,y):
+    score = font.render("Score : "+str(score_value),True,(255,255,255))
+    screen.blit(score,(x,y))
+
+def game_over_text():
+    over_text = over_font.render("GAME OVER",True,(255,255,255))
+    screen.blit(over_text,(200,250))
+
+def player(x,y):
+    screen.blit(playerImg,(x,y))
+
+def enemy(x,y,i):
+    screen.blit(enemyImg[i],(x,y))
+
+def fire_bullet(x,y):
+    global bullet_state
+    bullet_state = "fire"
+    screen.blit(bulletImg,(x+16,y+10))
+
+def isCollision(enemyX,enemyY,bulletX,bulletY):
+    distance = math.sqrt((enemyX-bulletX)**2+(enemyY - bulletY)**2)
+    return distance <COLLISION_DISTANCE
+
 
 
 
